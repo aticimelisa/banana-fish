2591bd0c46327f95b70374b70027179961f3b012
Melisa Atıcı <102025949+aticimelisa@users.noreply.github.com>
2025-04-01 17:40:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(trigger); // 👈 gözlenen artık trigger
   });
   
+function createFish() {
+  const fish = document.createElement('img');
+  fish.src = 'images/bananafish_menu.png';
+  fish.classList.add('swim-fish');
+
+  const size = Math.random() * 40 + 40;
+  fish.style.width = `${size}px`;
+  fish.style.top = `${Math.random() * 100}vh`;
+  const duration = Math.random() * 10 + 10;
+  fish.style.animationDuration = `${duration}s`;
+  fish.style.animationDelay = `${Math.random() * 5}s`;
+
+  document.getElementById('fishContainer').appendChild(fish);
+
+  setTimeout(() => fish.remove(), duration * 1000 + 5000);
+}
+
+if (document.getElementById('fishContainer')) {
+  setInterval(createFish, 1200);
+}
